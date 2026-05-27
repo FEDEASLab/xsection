c1aaa257211f925dc9b1b21089110a40030bc554
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-05-26 21:55:17 -0700

    Update test_a_rigid.py

--- tests/test_a_rigid.py
+++ tests/test_a_rigid.py
@@ -1,6 +1,7 @@
 
 import pytest
 from xsection.library import load_shape
+from xsection.analysis import SaintVenantSectionAnalysis
 
 def test_aisc_angle():
     shape = load_shape("L7x4x3/4", 
@@ -10,3 +11,16 @@ def test_aisc_angle():
                        mesh_type="T6")
     
     assert shape.area  == pytest.approx(7.74, rel=1e-2)
+
+def _test_aisc_angle_twist_center():
+    shape = load_shape("L7x4x3/4", 
+                       library="AISC16",
+                       mesh_scale=1/8, 
+                       mesher="gmsh",
+                       fillet=False,
+                       mesh_type="T6")
+    # sv = SaintVenantSectionAnalysis(shape)
+    # sy, sz = sv.twist_center()
+    sy, sz = shape._analysis.shear_center()
+    assert sy  == pytest.approx( 0, abs=0.1*shape.t)
+    assert sz  == pytest.approx( 0, abs=0.1*shape.t)
